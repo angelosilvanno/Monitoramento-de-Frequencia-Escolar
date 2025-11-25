c88f4f5fd25b79e40a59764981218681032d1408
JoaoPedroElric <joaopedroxbr14@gmail.com>
2025-11-25 12:35:16 -0300

    criando classe usuario

--- src/models/Usuario.java
+++ src/models/Usuario.java
@@ -1,5 +1,45 @@
 package Usuario;
-// teste
-public class Usuario{
-    
+
+public abstract class Usuario {
+    protected int idUsuario;
+    protected String nome;
+    protected String cpf;
+    protected String email;
+    protected String senha;
+
+    public Usuario(int idUsuario, String nome, String cpf, String email, String senha) {
+        this.idUsuario = idUsuario;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public int getId() {
+        return idUsuario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
