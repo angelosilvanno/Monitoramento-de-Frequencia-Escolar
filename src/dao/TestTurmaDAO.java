4e6ff22b028154ed52f1b6d80b4f9d03fc39323f
Ângelo <98216100+angelodesenvolvedor@users.noreply.github.com>
2025-11-29 17:33:40 -0300

    feat: simplificar o código no TestTurmaDAO e remover instância desnecessária do TurmaDAO

--- src/dao/TestTurmaDAO.java
+++ src/dao/TestTurmaDAO.java
@@ -1,24 +1,21 @@
 package dao;
 
 import models.Turma;
-import dao.TurmaDAO;
 
 public class TestTurmaDAO {
     public static void main(String[] args) {
         System.out.println("=== Teste de TurmaDAO ===");
 
-        TurmaDAO turmaDAO = new TurmaDAO();
-
         // Criar turma
-        Turma t = new Turma(3, "Ensino Médio");
-        TurmaDAO.criarTurma(t.getIdTurma(), t.getNomeTurma());
+        Turma turma = new Turma(3, "Ensino Médio");
+        TurmaDAO.criarTurma(turma.getIdTurma(), turma.getNomeTurma());
 
         System.out.println("Turma inserida com sucesso!");
 
         // Listar turmas
         System.out.println("\n--- Listando ---");
-        for (Turma turma : turmaDAO.listarTurma()) {
-            System.out.println(turma);
+        for (Turma t : TurmaDAO.listarTurma()) {
+            System.out.println(t);
         }
     }
 }
